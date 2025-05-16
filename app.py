2c886e96177e2606307545fc7614dc807d2811a3
vedhanayagan9626 <vedhanayagant2000@gmail.com>
2025-05-16 10:58:51 +0530

    Your update message here

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import base64
 import os
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 32* 1024 * 1024  # 32 MB limit
+
 UPLOAD_FOLDER = "static"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -12,14 +14,15 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    data_url = request.form['image']
+    data = request.get_json()
+    data_url = data['image']
     header, encoded = data_url.split(",", 1)
     binary_data = base64.b64decode(encoded)
-
+    
     filepath = os.path.join(UPLOAD_FOLDER, 'captured_image.png')
     with open(filepath, "wb") as f:
         f.write(binary_data)
-
+    print(f"Image saved to {filepath}")
     return 'Image received and saved!'
 
 if __name__ == '__main__':
